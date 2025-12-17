6c55832fa8ba906108c91042a62c05b174c82521
Maxim De Soomer <m.desoomer@xpofleet.com>
2025-12-17 13:28:47 +0100

    rewrite day 10 part 2 to quicker solution with bitwise operations and bfs

--- day10/factory.py
+++ day10/factory.py
@@ -1,42 +1,52 @@
-def push_button(state: list[int], wiring: list[int]):
-    for wire in wiring:
-        state[wire] = int(not state[wire])
+def to_mask(state):
+    state = state.strip("[]")
+    mask = 0
+    for i, v in enumerate(state):
+        if v == "#":
+            mask |= 1 << i
+    return mask
 
-    return state
+
+def button_mask(button):
+    button = button.strip("()").split(",")
+    mask = 0
+    for i in button:
+        mask ^= 1 << int(i)
+    return mask
 
 
 def find_minimum(
-    desired_state: list[int],
-    buttons: list[list[int]],
-    initial_states: list[list[int]],
-    presses: int = 0,
+    desired_state: int,
+    buttons: list[int],
 ):
-    presses += 1
-    new_states = []
-    for state in initial_states:
-        for button in buttons:
-            new_state = push_button(state.copy(), button)
-            if new_state == desired_state:
-                return presses
-            new_states.append(new_state)
-    return find_minimum(desired_state, buttons, new_states, presses)
+    observed_states = {0}
+    presses = 0
+    to_check = [(0, presses)]
+    while observed_states:
+        state, presses = to_check.pop()
+
+        if state == desired_state:
+            return presses
+
+        for b in buttons:
+            new_state = state ^ b  # bitwise xor
+            if new_state not in observed_states:
+                observed_states.add(new_state)
+                to_check.insert(0, (new_state, presses + 1))
 
 
 machines = []
 with open("input/input.txt", "r") as file:
     for line in file.readlines():
         items = line.split()
-        ind_lights = [int(x == "#") for x in items[0][1:-1]]
-        buttons = [
-            [int(x) for x in item.strip("()").split(",")] for item in items[1:-1]
-        ]
+        ind_lights = to_mask(items[0])
+        buttons = [button_mask(b) for b in items[1:-1]]
         machines.append((ind_lights, buttons))
 
 
 minima = []
 for machine in machines:
-    initial_state = [0 for _ in range(len(machine[0]))]
-    minimum_presses = find_minimum(machine[0], machine[1], [initial_state])
-    minima.append(minimum_presses)
+    minimum = find_minimum(machine[0], machine[1])
+    minima.append(minimum)
 
 print(sum(minima))
