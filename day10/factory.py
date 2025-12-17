3748e22168cb8b6fadecd4d826b960f223ec090d
Maxim De Soomer <m.desoomer@xpofleet.com>
2025-12-17 12:42:13 +0100

    add slow solution to part 10

--- day10/factory.py
+++ day10/factory.py
@@ -1,25 +1,42 @@
-def push_button(initial_state: tuple[int, ...], wiring: tuple[int, ...]):
-    new_state = []
-    for i in wiring:
-        new_state[i] = initial_state[i] % 2
+def push_button(state: list[int], wiring: list[int]):
+    for wire in wiring:
+        state[wire] = int(not state[wire])
 
-    return tuple(new_state)
+    return state
+
+
+def find_minimum(
+    desired_state: list[int],
+    buttons: list[list[int]],
+    initial_states: list[list[int]],
+    presses: int = 0,
+):
+    presses += 1
+    new_states = []
+    for state in initial_states:
+        for button in buttons:
+            new_state = push_button(state.copy(), button)
+            if new_state == desired_state:
+                return presses
+            new_states.append(new_state)
+    return find_minimum(desired_state, buttons, new_states, presses)
 
 
 machines = []
-with open("input/sample.txt", "r") as file:
+with open("input/input.txt", "r") as file:
     for line in file.readlines():
         items = line.split()
-        ind_lights = tuple(int(x == "#") for x in items[0][1:-1])
-        buttons = tuple(button for button in x for x in items[1:-1])
+        ind_lights = [int(x == "#") for x in items[0][1:-1]]
+        buttons = [
+            [int(x) for x in item.strip("()").split(",")] for item in items[1:-1]
+        ]
         machines.append((ind_lights, buttons))
 
 
-print(machines[0][0])
-print(machines[0][1])
+minima = []
+for machine in machines:
+    initial_state = [0 for _ in range(len(machine[0]))]
+    minimum_presses = find_minimum(machine[0], machine[1], [initial_state])
+    minima.append(minimum_presses)
 
-# for machine in machines:
-#     size = len(machine[0])
-#     indicators = tuple(map(lambda x: int(x == "#"), machine[0]))
-#     buttons = machines[0]
-#     tuple(0 for )
+print(sum(minima))
